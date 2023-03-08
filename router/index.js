86385a18a71bfa836658372f87f39c30accb65ed
Bryan Gonzalez <2019030292@upsin.edu.mx>
2023-03-07 17:08:38 -0700

    Generar Router

---
+++ router/index.js
@@ -0,0 +1,18 @@
+// Importar el módulo body-parser y el framework Express
+const bodyParser = require("body-parser");
+const express = require("express");
+
+// Crear una instancia de Router
+const router = express.Router();
+
+// Ruta principal que muestra una página con los datos del array "datos"
+router.get("/", (req, res) => {
+  res.render("index.ejs", {
+    Header: "Sistemas Microinformatica",
+    subHeader: "Recursos Humanos",
+    Footer: "Compañia de luz @Derechos Reservados 2023 Bryan Gonzalez",
+  });
+});
+
+// Exportar el módulo Router para que pueda ser utilizado en otros archivos
+module.exports = router;
